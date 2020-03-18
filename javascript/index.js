41d0a3eea8653544ffd4ec88aea84f28b3f8824a
pablo <taddeiApablo23@gmail.com>
2020-03-18 10:49:57 -0300

    nuevos cambios
    terminando mi webiste

--- javascript/index.js
+++ javascript/index.js
@@ -12,13 +12,14 @@ $(function(){
         $.magnificPopup.open({
             items: [
                 {
-                  src: 'img/projects/net/AppVideoClub.png'
+                  src: 'img/projects/net/AppVideoClub.png',
+                  title: '.Net Framework : App realizada con winForm, administracion de un videoClub'
                 }
               ],
               gallery: {
                 enabled: true
               },
-              type: 'image' // this is default type
+              type: 'image',
         });
      });
 
@@ -26,16 +27,20 @@ $(function(){
         $.magnificPopup.open({
             items: [
                 {
-                    src: 'img/projects/html/BSAS-APP.png'
+                    src: 'img/projects/html/BSAS-APP.png',
+                    title: 'Ionic: App hibrida para android, localiza los distintos servicios dentro de la ciudad BSAS'
                 },
                 {
-                    src: 'img/projects/html/BSAS-APP1.png'
+                    src: 'img/projects/html/BSAS-APP1.png',
+                    title: 'Ionic: otra toma de la misma app hibrida (tablet)'
                 },
                 {
-                    src: 'img/projects/html/BSAS-APP2.png'
+                    src: 'img/projects/html/BSAS-APP2.png',
+                    title: 'Ionic: otra toma de la misma app hibrida (mobile)'
                 },
                 {
-                    src: 'img/projects/html/angularjs.png'
+                    src: 'img/projects/html/angularjs.png',
+                    title: 'Angularjs: aplicacion web utilizando este framework de javascripts'
                 }
 
             ],
@@ -50,16 +55,20 @@ $(function(){
         $.magnificPopup.open({
             items: [
                 {
-                    src: 'img/projects/blender/crylers.png'
+                    src: 'img/projects/blender/crylers.png',
+                    title: 'Blender : modelado de un crysler imperial 1933'
                 },
                 {
-                    src: 'img/projects/blender/chevrolet-pickup.png'
+                    src: 'img/projects/blender/chevrolet-pickup.png',
+                    title: 'Blender : modelado de un Chevrolet Pickup 1951'
                 },
                 {
-                    src: 'img/projects/blender/bentley-mk6.png'
+                    src: 'img/projects/blender/bentley-mk6.png',
+                    title: 'Blender : modelado de un bentley mk6'
                 },
                 {
-                    src: 'img/projects/blender/pampa3.png'
+                    src: 'img/projects/blender/pampa3.png',
+                    title: 'Blender : modelado de un IA 64 PAMPA 3'
                 }
 
             ],
